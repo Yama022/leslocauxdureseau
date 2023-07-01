6c5fb5c829c69ff72c07cce57b27aa9f0ba3948d
Remi Jacquemin <79313646+Yama022@users.noreply.github.com>
2023-07-01 17:04:22 +0200

    Update name to Les locaux du réseau / change title name

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 
-import './App.scss';
+import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App__header">
+        <h1>Les locaux du réseau</h1>
       </header>
+
+      <main className='App__main'>
+        <div>
+          <h2>Test</h2>
+        </div>
+      </main>
     </div>
   );
 }
